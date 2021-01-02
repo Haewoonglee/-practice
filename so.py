c6f3b8c88d5958737f2b5692760f88f53c2c586a
Haewoong <dalgoo9741@gmail.com>
2021-01-02 15:37:24 +0000

    Scrapping Program

--- so.py
+++ so.py
@@ -15,13 +15,18 @@ def get_last_page():
 def extract_job(html):
   title = html.find("h2", {"class":"mb4"}).find("a")["title"]
   company, location = html.find("h3",{"class":"mb4"}).find_all("span", recursive=False)
-  print(company.get_text(strip=True), location.get_text(strip=True))
-  return {'title':title}
+  company = company.get_text(strip=True)
+  location = location.get_text(strip=True).strip("-").strip(" \r").strip("\n")
+  job_id = html["data-jobid"]
+
+  return {'title':title, 'company':company, 'location':location,
+  'apply_link':f"https://stackoverflow.com/jobs/{job_id}"}
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO: Page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div",{"class":"-job"})
