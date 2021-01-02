3141c1df513f8eb59ac0e33c6364f5c6d5c89e90
Haewoong <dalgoo9741@gmail.com>
2021-01-02 14:50:05 +0000

    v10.2

--- so.py
+++ so.py
@@ -12,15 +12,23 @@ def get_last_page():
     last_page = pages[-2].get_text(strip=True)
     return int(last_page)
 
+def extract_job(html):
+  title =  html.find("h2", {"class":"mb4"}).find("a")["title"]
+  company_row = html.find("div", {"class":"-company"}).find_all("span")
+  print(company_row)
+  return{'title' : title}
+
 
 def extract_job(last_page):
-    jobs = []
-    for page in range(last_page):
-        result = requests.get(f"{URL}&pg={page+1}")
-        soup = BeautifulSoup(result.text, "html.parser")
-        results = soup.find_all("div",{"class":"-job"})
-        for result in results:
-          print(result["data-jobid"])
+  jobs = []
+  for page in range(last_page):
+    result = requests.get(f"{URL}&pg={page+1}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div",{"class":"-job"})
+    for result in results:
+      job = extract_job(result)
+      job.append(job)
+  return jobs
 
 
 def get_jobs():
